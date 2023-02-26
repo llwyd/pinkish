correct formula for single pole lpf

python/dsp.py
@@ -70,8 +70,6 @@ def db_gain( g ):
     return -20*np.log10(g)
 
 def get_alpha( fc, fs ):
-    # Wikipedia
-    #alpha = 1 / ( (1/(2*np.pi*(fc/fs))) + 1)
     alpha = np.exp( -2 * np.pi * ( fc / fs ) )
     return alpha
 
@@ -79,7 +77,7 @@ def ewma( alpha, length ):
     dirac = signal.unit_impulse( length )
     y = np.zeros( length )
     for i in range( length ):
-        y[i] = dirac[i] + ( alpha * ( dirac[i] - y[i-1] ) )
+        y[i] = dirac[i] - alpha*(dirac[i] - y[i-1])
     return y
 
 def generate_decade_line(start_mag, end_freq):