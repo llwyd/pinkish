b891347b9a276f31c5c2a2ee3c9dd13a19af5c92
llwyd <tom@llwyd.io>
2023-04-02 20:44:14 +0100

    more adjustments

--- python/q_arithmetic.py
+++ python/q_arithmetic.py
@@ -10,7 +10,7 @@ import noise
 from enum import Enum
 
 int_type = np.uint32
-max_exp = 16
+max_exp = 32
 
 def convert_to_q(num, q) -> np.uint32:
     assert( q < max_exp )
@@ -25,26 +25,28 @@ def q_to_float(num, q):
     return result
 
 
-q = 4
-a = 0.666
-a_q  = convert_to_q(a,q)
-q_to_float(a_q,q)
 
-print("")
-a = -0.666
-a_q  = convert_to_q(a,q)
-q_to_float(a_q,q)
+if __name__ == "__main__":
+    q = 31
+    a = 0.666
+    a_q  = convert_to_q(a,q)
+    q_to_float(a_q,q)
+
+    print("")
+    a = -0.666
+    a_q  = convert_to_q(a,q)
+    q_to_float(a_q,q)
 
 
-num_samples = 4096
-fs = 48000
-f = 50
-t = np.linspace(0,num_samples - 1, num_samples)
-x = np.sin( 2 * np.pi * f * t / fs )
+    num_samples = 4096
+    fs = 48000
+    f = 50
+    t = np.linspace(0,num_samples - 1, num_samples)
+    x = np.sin( 2 * np.pi * f * t / fs ) * 0.9999999
 
-y = convert_to_q(x,15)
-plt.plot(y)
-plt.show()
+    y = convert_to_q(x,q)
+    plt.plot(y)
+    plt.show()
 
 
 
