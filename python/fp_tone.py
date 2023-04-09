e0a30d785f7ec095b5ce80d8fe0c53cd26fd4efd
llwyd <tom@llwyd.io>
2023-04-09 23:28:02 +0100

    add cos and sine updates

--- python/fp_tone.py
+++ python/fp_tone.py
@@ -6,12 +6,12 @@ import dsp
 import q_arithmetic as q_math
 
 
-num_samples = 4096
+num_samples = 400
 fs = 44100
 f = 100
 
-phi = 0.5
-delta = 0.1
+phi = 0.0
+delta = 0.4
 
 s = np.sin(phi)
 c = np.cos(phi)
@@ -20,5 +20,18 @@ s_delta = np.sin(delta)
 c_delta = np.cos(delta)
 
 # 0.5 + 0.1
-y = s*c_delta + c*s_delta
+x_s = s*c_delta + c*s_delta
+x_c = c*c_delta - s*s_delta
+
+y_s = np.zeros(num_samples)
+y_c = np.zeros(num_samples)
+
+y_s[0] = x_s
+y_c[0] = x_c
+
+for i in range(1,num_samples):
+
+    y_s[i] = y_s[i-1]*c_delta + y_c[i-1]*s_delta
+    y_c[i] = y_c[i-1]*c_delta - y_s[i-1]*s_delta
+
 
