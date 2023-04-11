02f7204b50bf87378aff777adc9d739d51115a9a
llwyd <tom@llwyd.io>
2023-04-11 20:40:58 +0100

    more tinkering with resonators

--- python/fp_resonator.py
+++ python/fp_resonator.py
@@ -9,9 +9,9 @@ import q_arithmetic as q_math
 num_samples = 4096
 fs = 44100
 f = 100
-A = 0.9999;
+A = 0.95;
 
-q = 14
+q = 13
 
 w = ( 2*np.pi*f )/ fs
 
@@ -33,12 +33,13 @@ y_q = q_math.float_to_q16bit(y,q)
 for i in range(num_samples):
     y[i] = ( x[i] * b0 ) - (a1*y[i-1]) - (a2 * y[i-2])
 
-    temp_0 = np.int16((np.int32(x_q[i]) * np.int32(b0_q)) >> q)
-    temp_1 = np.int16((np.int32(y_q[i-1]) * np.int32(a1_q)) >> q)
-    temp_2 = np.int16((np.int32(y_q[i-2]) * np.int32(a2_q)) >> q)
+    temp_0 = q_math.mul16(x_q[i],b0_q,q)
+    temp_1 = q_math.mul16(y_q[i-1],a1_q,q)
+    temp_2 = q_math.mul16(y_q[i-2],a2_q,q)
 
-    y_q[i] = temp_0 - temp_1
-    y_q[i] = y_q[i] - temp_2
+    temp_3 = temp_0 - temp_1
+    temp_4 = temp_3 - temp_2
+    y_q[i] = temp_4
 
 plt.subplot(2,1,1)
 plt.plot(y)
